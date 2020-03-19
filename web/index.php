91679d2923651428eb2b7c2174d8d1899c2938d9
boonhong <ganboonhong@gmail.com>
2020-03-19 14:03:25 +0800

    make console app read dotenv config

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ use Dotenv\Dotenv;
 require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
+echo 123;exit;
 defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV']);
 defined('YII_DEBUG') or define('YII_DEBUG', $_ENV['YII_ENV'] === 'dev');
 
