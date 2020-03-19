a02b4493d2226c405eca1a5b4f203b44dec8231d
boonhong <ganboonhong@gmail.com>
2020-03-19 11:02:58 +0800

    utilize dotenv package

--- web/index.php
+++ web/index.php
@@ -1,10 +1,13 @@
 <?php
-
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+use Dotenv\Dotenv;
 
 require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV']);
+defined('YII_DEBUG') or define('YII_DEBUG', $_ENV['YII_ENV'] === 'dev');
+
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
