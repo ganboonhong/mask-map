f6812e2caec2ae92cfb486d1fdefd542efc81bcb
boonhong <ganboonhong@gmail.com>
2020-03-23 13:33:56 +0800

    remove index page redirect

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -61,8 +61,7 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->redirect('pharmacy/map');
-        // return $this->render('index');
+        return $this->render('index');
     }
 
     /**
--- web/index.php
+++ web/index.php
@@ -1,11 +1,8 @@
 <?php
-use Dotenv\Dotenv;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
+$_ENV = getenv();
 defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV']);
 defined('YII_DEBUG') or define('YII_DEBUG', $_ENV['YII_ENV'] === 'dev');
 
