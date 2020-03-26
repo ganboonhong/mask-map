d2dbfd8cbfd5c39600dd61a05f7c8d5ed96c689e
boonhong <ganboonhong@gmail.com>
2020-03-26 23:19:32 +0800

    test: fix test case

--- tests/unit/models/PharmacyTest.php
+++ tests/unit/models/PharmacyTest.php
@@ -11,6 +11,6 @@ class PharmacyTest extends Unit
         $pharmacy = $this->make(new Pharmacy);
         $pharmacy = new Pharmacy;
         $actual = $pharmacy->sync();
-        $this->assertIsNotInt($actual);
+        $this->assertIsInt($actual);
     }
 }
