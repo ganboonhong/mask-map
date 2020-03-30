97b32dab23704aba48ae127af40f384a8780604c
boonhong <ganboonhong@gmail.com>
2020-03-30 22:31:42 +0800

    test merging PR to trigger webhook

--- controllers/GithubController.php
+++ controllers/GithubController.php
@@ -6,10 +6,6 @@ use yii\web\Controller;
 
 class GithubController extends Controller
 {
-    public function actionFoo()
-    {
-        echo 123;
-    }
     public function actionWebhook()
     {
         $data = json_decode(file_get_contents('php://input'), true);
