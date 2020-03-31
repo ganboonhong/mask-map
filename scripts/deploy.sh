555412b8030a6346bee092ff1c147a01cbca52ae
boonhong <ganboonhong@gmail.com>
2020-03-31 21:50:33 +0800

    modify deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,8 @@
 
 cd /var/www/html/my-lab/mask-map \
 # cd ~/mask-map \
-&& git checkout master \
-&& git pull \
-&& echo "composer installing" \
-&& composer install --no-dev \
-&& echo "deployed successfully"
+git checkout master && \
+git pull && \
+echo "composer installing" && \
+composer install --no-dev && \
+echo "deployed successfully"
