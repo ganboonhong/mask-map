f0ddbeab0a0e51eee93a8fe520030c25af1509b4
boonhong <ganboonhong@gmail.com>
2020-03-31 10:10:03 +0800

    test webhook

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Deploying application"
+
+# cd /var/www/html/my-lab/mask-map
+cd ~/mask-map \
+&& git checkout master \
+&& git pull \
+&& echo "composer installing" \
+&& composer install --no-dev \
+&& echo "deployed successfully"
